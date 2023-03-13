46c24e6b0445b5d97121a38e691d3dd5e1944919
mutish <mutindastacey@gmail.com>
2023-03-13 10:00:11 +0300

    "update"

--- Enterprise/urls.py
+++ Enterprise/urls.py
@@ -4,11 +4,15 @@ from shoe import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('shoe.urls')),
     path('new', views.new),
     path('show', views.show),
     path('edit/<int:id>', views.edit),
     path('update/<int:id>', views.update),
+    path('checkout/<int:id>', views.checkout, name='checkout'),
     path('delete/<int:id>', views.destroy),
-    path('',include('shoe.urls')),
     path('', views.show)
 ]
+
+
+
--- shoe/views.py
+++ shoe/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render, redirect
-from django_daraja.views import stk_push_callback_url
-
-from shoe.forms import ShoesForm
-from shoe.models import Shoes
-from django.http import HttpResponse
+from __future__ import unicode_literals
+from django.http import HttpResponseNotAllowed
+from django.http import HttpResponse, JsonResponse
 from django_daraja.mpesa.core import MpesaClient
 from decouple import config
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from django_daraja.views import stk_push_callback_url
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from shoe.forms import ShoesForm
+from shoe.models import Shoes
+
+
+
 
 # Create your views here.
 def new(request):
@@ -32,20 +37,46 @@ def edit(request, id):
 
 def update(request, id):
     shoes = Shoes.objects.get(id=id)
-    form = Shoes(request.POST, instance=shoes)
+    form = ShoesForm(request.POST, instance=shoes)
     if form.is_valid():
         form.save()
         return redirect('/show')
 
     return redirect(request, 'edit.html', {'shoes': shoes})
 
-def index(request):
-    shoes = Shoes.objects.get(id=id)
-    form = Shoes(request.POST, instance=shoes)
-    if form.is_valid():
-        form.save()
-        return redirect('/show')
-    return redirect(request, 'checkout.html', {'shoes': shoes})
+def checkout(request, id):
+    if request.method == 'POST':
+        price = request.POST.get('shoe_price')
+        phone_number = request.POST.get('phone_number')
+        shoe_id = id  # set the shoe ID to the specified ID
+        cl = MpesaClient()
+        account_reference = 'Enterprise Shoes'
+        transaction_desc = 'Paying for shoes'
+        callback_url = request.build_absolute_uri(reverse('payment_callback'))  # use a dynamic callback URL
+        try:
+            response = cl.stk_push(phone_number, price, account_reference, shoe_id, transaction_desc, callback_url)
+            return JsonResponse(response.response_description, safe=False)
+        except:
+            return JsonResponse({'message': 'An error occurred while processing your request. Please try again.'}, status=500)
+    else:
+        shoe = Shoes.objects.get(id=id)
+        return render(request, 'checkout.html', {'shoe': shoe})
+
+
+
+@csrf_exempt
+def payment_callback(request):
+    if request.method == 'POST':
+        # extract the payment details from the request
+        # verify the payment using the Safaricom API
+        # update the payment status in your application
+        return HttpResponse('Payment received.')
+    else:
+        return HttpResponseNotAllowed(['POST'])
+
+
+
+
 
 def  stk_push_success(request):
     cl = MpesaClient()
@@ -56,26 +87,6 @@ def  stk_push_success(request):
     callback_url = stk_push_callback_url
     r = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
     return JsonResponse(r.response_description, safe=False)
-
-
-
-
-# def index(request, id):
-#     shoes = Shoes.objects.get(id=id)
-#     form = Shoes(request.POST, instance=shoes)
-#     if form.is_valid():
-#         form.save()
-#         return redirect('/show')
-#     return redirect(request, 'checkout.html', {'shoes': shoes})
-#
-#     cl = MpesaClient()
-#     phone_number = '0745353600'
-#     amount = 1
-#     account_reference = 'ENTERPRISES'
-#     transaction_desc = 'paying shoes'
-#     callback_url = 'https://api.darajambili.com/express-payment'
-#     response = cl.stk_push(phone_number, amount, account_reference, transaction_desc, callback_url)
-#     return HttpResponse(response)
 
 
 def destroy(request, id):
